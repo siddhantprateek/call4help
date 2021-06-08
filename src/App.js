b75220c9dd63c274692ae40c153eb855eb0d899e
siddhantprateek <siddhantprateek@gmail.com>
2021-06-08 15:39:10 +0530

    contents and images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import image_1 from "./img/content1.png";
+import image_2 from "./img/content2.png";
+import image_3 from "./img/content3.png";
+import image_4 from "./img/content4.png";
 import './App.css';
+import React, { useState } from "react";
 
 
 function App() {
+  const [cursorX, setCursorX] = useState()
+  const [cursorY, setCursorY] = useState()
+
+  window.addEventListener('mousemove', (e) => {
+    setCursorX(e.pageX)
+    setCursorY(e.pageY)
+  })
+
+
+
   return (
     <div className="App">
+      {/* <div class="cursor-follower"></div> */}
       <div className="main">
         <div className="logo">
           <img src="" alt="" />
@@ -21,11 +36,19 @@ function App() {
             <li>
               <a href="@">Sign Up | login</a>
             </li>
+          <div className="cursor" style={{
+                  left: cursorX + 'px',
+                  top: cursorY + 'px'
+          }}>
+          </div>
           </ul>
         </div>
 
         <div className="img-container">
-          <img src="" alt="" />
+          <img src={image_1} className="img_1" alt="" />
+          <img src={image_2} className="img_2" alt="" />
+          <img src={image_3} className="img_3" alt="" />
+          <img src={image_4} className="img_4" alt="" />
         </div>
       </div>
     </div>
---
+++ src/about/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./about.css";
+
+
+function About() {
+
+    return (
+        <div className="About">
+            <div className="content">
+                <p>
+                    This is a community run project, Which help in finding people who need help and get help from others.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
